35cfbc95b9f1e4c631c3453beafa63bb44c08ed4
Lucas <lucascardiaskrambeck2025@gmail.com>
2026-02-12 22:19:30 -0300

    preparando deploy render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "Sistema da Assistência Técnica funcionando!"
+    return "Sistema da LCK Tecnologia Online 🚀"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
